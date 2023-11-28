9ca048dec03d1e791347545541a3d413d79a6859
3ricc <zericcz@gmail.com>
2023-11-28 04:34:30 -0600

    changes

--- main.go
+++ main.go
@@ -58,30 +58,37 @@ func main() {
 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 	}()
 
-    collectGithubData(db)
+    collectPrometheusData(db)
+    collectSeleniumData(db)
+    collectOpenAIData(db)
+    collectDockerData(db)
+    collectMilvusData(db)
+    collectGoData(db)
 
 
 }
 
-func collectGithubData (db *sql.DB) error {
+func collectPrometheusData (db *sql.DB) error {
 
-    // drop_table := `drop table if exists prometheus_issues`
+    drop_table := `drop table if exists prometheus_issues`
 
-	// _, err := db.Exec(drop_table)
-	// if err != nil {
-	// 	panic(err)
-	// }
-    // create_table := `CREATE TABLE IF NOT EXISTS "prometheus_issues" (
-    //                     "title"  VARCHAR(255),
-    //                     "id"        INT,
-    //                     "authorID"  INT,
-    //                     "creationDate"  VARCHAR(255),
-    //                     PRIMARY KEY("id")
-    //                 );`
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "prometheus_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
 
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
 
-
-    req, err := http.Get("https://api.github.com/repos/microsoft/typescript/issues")
+    req, err := http.Get("https://api.github.com/repos/prometheus/prometheus/issues")
     if err != nil {
 		panic(err)
 	}
@@ -95,18 +102,265 @@ func collectGithubData (db *sql.DB) error {
         var creationDate = prometheuslist[i].Creation
         var issue_id = prometheuslist[i].IssueID
 
-        fmt.Printf(issue_name)
-        fmt.Printf(creationDate)
-        fmt.Printf(":%s", issue_id)
+        sql := `INSERT INTO prometheus_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
 
-        //sql := `INSERT INTO prometheus_issues ("issue name", "author", "creationDate")`
+		if err != nil {
+			log.Fatalf("Error on prometheus insertion: %s", err.Error())
+		}
 
     }
-
     return nil
-
 }
 
+func collectSeleniumData (db *sql.DB) error {
+
+    drop_table := `drop table if exists selenium_issues`
+
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "selenium_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.github.com/repos/seleniumhq/selenium/issues")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var seleniumlist GitHubIssueStruct
+	json.Unmarshal(body, &seleniumlist)
+
+    for i:= 0; i < len(seleniumlist); i++ {
+        var issue_name = seleniumlist[i].Title
+        var creationDate = seleniumlist[i].Creation
+        var issue_id = seleniumlist[i].IssueID
+
+        sql := `INSERT INTO selenium_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
+
+		if err != nil {
+			log.Fatalf("Error on selenium insertion: %s", err.Error())
+		}
+
+    }
+    return nil
+}
+
+func collectOpenAIData (db *sql.DB) error {
+
+    drop_table := `drop table if exists openai_issues`
+
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "openai_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.github.com/repos/openai/openai-python/issues")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var openailist GitHubIssueStruct
+	json.Unmarshal(body, &openailist)
+
+    for i:= 0; i < len(openailist); i++ {
+        var issue_name = openailist[i].Title
+        var creationDate = openailist[i].Creation
+        var issue_id = openailist[i].IssueID
+
+        sql := `INSERT INTO openai_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
+
+		if err != nil {
+			log.Fatalf("Error on openai insertion: %s", err.Error())
+		}
+
+    }
+    return nil
+}
+
+func collectDockerData (db *sql.DB) error {
+
+    drop_table := `drop table if exists docker_issues`
+
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "docker_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.github.com/repos/docker/cli/issues")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var dockerlist GitHubIssueStruct
+	json.Unmarshal(body, &dockerlist)
+
+    for i:= 0; i < len(dockerlist); i++ {
+        var issue_name = dockerlist[i].Title
+        var creationDate = dockerlist[i].Creation
+        var issue_id = dockerlist[i].IssueID
+
+        sql := `INSERT INTO docker_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
+
+		if err != nil {
+			log.Fatalf("Error on docker insertion: %s", err.Error())
+		}
+
+    }
+    return nil
+}
+
+func collectMilvusData (db *sql.DB) error {
+
+    drop_table := `drop table if exists milvus_issues`
+
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "milvus_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.github.com/repos/milvus-io/milvus/issues")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var milvuslist GitHubIssueStruct
+	json.Unmarshal(body, &milvuslist)
+
+    for i:= 0; i < len(milvuslist); i++ {
+        var issue_name = milvuslist[i].Title
+        var creationDate = milvuslist[i].Creation
+        var issue_id = milvuslist[i].IssueID
+
+        sql := `INSERT INTO milvus_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
+
+		if err != nil {
+			log.Fatalf("Error on milvus insertion: %s", err.Error())
+		}
+
+    }
+    return nil
+}
+
+func collectGoData (db *sql.DB) error {
+
+    drop_table := `drop table if exists go_issues`
+
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "go_issues" (
+                        "title"  VARCHAR(255),
+                        "id"        INT,
+                        "creationDate"  VARCHAR(255),
+                        PRIMARY KEY("id")
+                    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.github.com/repos/golang/go/issues")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var golist GitHubIssueStruct
+	json.Unmarshal(body, &golist)
+
+    for i:= 0; i < len(golist); i++ {
+        var issue_name = golist[i].Title
+        var creationDate = golist[i].Creation
+        var issue_id = golist[i].IssueID
+
+        sql := `INSERT INTO go_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			issue_name,
+            issue_id,
+            creationDate)
+
+		if err != nil {
+			log.Fatalf("Error on go insertion: %s", err.Error())
+		}
+
+    }
+    return nil
+}
 
 // func collectStackOverflowData (db *sql.DB) error{
 
