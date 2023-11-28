d45b13235cdd95c1cdf4df75e30be6088281d94e
3ricc <zericcz@gmail.com>
2023-11-28 05:50:57 -0600

    testing stackoverflow

--- main.go
+++ main.go
@@ -8,10 +8,8 @@ import (
     "os"
     "net/http"
     "time"
-    //"context"
     _ "github.com/lib/pq"
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
-    //"github.com/google/go-github/v56/github"
     "io/ioutil"
     
 )
@@ -20,6 +18,14 @@ type GitHubIssueStruct []struct {
 	Title                      string `json:"title"`
     IssueID                    int    `json:"id"`
 	Creation                   string `json:"created_at"`
+}
+
+type StackOverflowPosts struct{
+    Items [] struct{
+        QuestionID int `json:"question_id"`
+        Creation int `json:"creation_date"`
+        Title string `json:"title"`
+    } `json:"items"`
 }
 
 func main() {
@@ -60,15 +66,23 @@ func main() {
 	}()
 
     for {
+        //GitHub Data
         collectPrometheusData(db)
         collectSeleniumData(db)
         collectOpenAIData(db)
         collectDockerData(db)
         collectMilvusData(db)
         collectGoData(db)
+
+        //StackOverflowData
+        collectPrometheusStack(db)
+        collectSeleniumStack(db)
+        collectOpenaiPosts(db)
+        collectDockerPosts(db)
+        collectMilvusPosts(db)
+        collectGoPosts(db)
         time.Sleep(24 * time.Hour)
     }
-    
     
 
 }
@@ -84,7 +98,7 @@ func collectPrometheusData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "prometheus_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -107,7 +121,7 @@ func collectPrometheusData (db *sql.DB) error {
         var creationDate = prometheuslist[i].Creation
         var issue_id = prometheuslist[i].IssueID
 
-        sql := `INSERT INTO prometheus_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO prometheus_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -133,7 +147,7 @@ func collectSeleniumData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "selenium_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -156,7 +170,7 @@ func collectSeleniumData (db *sql.DB) error {
         var creationDate = seleniumlist[i].Creation
         var issue_id = seleniumlist[i].IssueID
 
-        sql := `INSERT INTO selenium_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO selenium_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -182,7 +196,7 @@ func collectOpenAIData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "openai_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -205,7 +219,7 @@ func collectOpenAIData (db *sql.DB) error {
         var creationDate = openailist[i].Creation
         var issue_id = openailist[i].IssueID
 
-        sql := `INSERT INTO openai_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO openai_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -231,7 +245,7 @@ func collectDockerData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "docker_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -254,7 +268,7 @@ func collectDockerData (db *sql.DB) error {
         var creationDate = dockerlist[i].Creation
         var issue_id = dockerlist[i].IssueID
 
-        sql := `INSERT INTO docker_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO docker_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -280,7 +294,7 @@ func collectMilvusData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "milvus_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -303,7 +317,7 @@ func collectMilvusData (db *sql.DB) error {
         var creationDate = milvuslist[i].Creation
         var issue_id = milvuslist[i].IssueID
 
-        sql := `INSERT INTO milvus_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO milvus_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -329,7 +343,7 @@ func collectGoData (db *sql.DB) error {
     create_table := `CREATE TABLE IF NOT EXISTS "go_issues" (
                         "title"  VARCHAR(255),
                         "id"        INT,
-                        "creationDate"  VARCHAR(255),
+                        "creation_date"  VARCHAR(255),
                         PRIMARY KEY("id")
                     );`
 
@@ -352,7 +366,7 @@ func collectGoData (db *sql.DB) error {
         var creationDate = golist[i].Creation
         var issue_id = golist[i].IssueID
 
-        sql := `INSERT INTO go_issues ("title", "id", "creationDate") values($1, $2, $3)`
+        sql := `INSERT INTO go_issues ("title", "id", "creation_date") values($1, $2, $3)`
         _, err = db.Exec(
 			sql,
 			issue_name,
@@ -367,6 +381,296 @@ func collectGoData (db *sql.DB) error {
     return nil
 }
 
-// func collectStackOverflowData (db *sql.DB) error{
+func collectPrometheusStack (db *sql.DB) error{
+    drop_table := `drop table if exists prometheus_posts`
 
-// }
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "prometheus_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=prometheus&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var promStackList StackOverflowPosts
+	json.Unmarshal(body, &promStackList)
+
+    for i := 0; i < len(promStackList.Items); i++{
+        var question_name = promStackList.Items[i].Title
+        var creation_date = promStackList.Items[i].Creation
+        var question_id = promStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO prometheus_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on prometheus stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
+
+func collectSeleniumStack (db *sql.DB) error{
+    drop_table := `drop table if exists selenium_posts`
+
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "selenium_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=selenium&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var seleniumStackList StackOverflowPosts
+	json.Unmarshal(body, &seleniumStackList)
+
+    for i := 0; i < len(seleniumStackList.Items); i++{
+        var question_name = seleniumStackList.Items[i].Title
+        var creation_date = seleniumStackList.Items[i].Creation
+        var question_id = seleniumStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO selenium_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on selenium stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
+
+func collectOpenaiPosts (db *sql.DB) error{
+    drop_table := `drop table if exists openai_posts`
+
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "openai_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=openai-api&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var openStackList StackOverflowPosts
+	json.Unmarshal(body, &openStackList)
+
+    for i := 0; i < len(openStackList.Items); i++{
+        var question_name = openStackList.Items[i].Title
+        var creation_date = openStackList.Items[i].Creation
+        var question_id = openStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO openai_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on openai stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
+
+func collectDockerPosts (db *sql.DB) error{
+    drop_table := `drop table if exists docker_posts`
+
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "docker_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=docker&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var dockerStackList StackOverflowPosts
+	json.Unmarshal(body, &dockerStackList)
+
+    for i := 0; i < len(dockerStackList.Items); i++{
+        var question_name = dockerStackList.Items[i].Title
+        var creation_date = dockerStackList.Items[i].Creation
+        var question_id = dockerStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO docker_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on docker stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
+
+func collectMilvusPosts (db *sql.DB) error{
+    drop_table := `drop table if exists milvus_posts`
+
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "milvus_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=milvus&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var milvusStackList StackOverflowPosts
+	json.Unmarshal(body, &milvusStackList)
+
+    for i := 0; i < len(milvusStackList.Items); i++{
+        var question_name = milvusStackList.Items[i].Title
+        var creation_date = milvusStackList.Items[i].Creation
+        var question_id = milvusStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO milvus_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on milvus stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
+
+func collectGoPosts (db *sql.DB) error{
+    drop_table := `drop table if exists go_posts`
+
+    _, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+    create_table := `CREATE TABLE IF NOT EXISTS "go_posts" (
+        "title"  VARCHAR(255),
+        "id"        INT,
+        "creation_date"  INT,
+        PRIMARY KEY("id")
+    );`
+
+    _, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+    req, err := http.Get("https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=golang&site=stackoverflow&key=c5hL2NXUJ*1TIoPb27Qudg((")
+    if err != nil {
+		panic(err)
+	}
+
+    body, _ := ioutil.ReadAll(req.Body)
+	var goStackList StackOverflowPosts
+	json.Unmarshal(body, &goStackList)
+
+    for i := 0; i < len(goStackList.Items); i++{
+        var question_name = goStackList.Items[i].Title
+        var creation_date = goStackList.Items[i].Creation
+        var question_id = goStackList.Items[i].QuestionID
+
+        sql := `INSERT INTO go_posts ("title", "id", "creation_date") values($1, $2, $3)`
+        _, err = db.Exec(
+			sql,
+			question_name,
+            question_id,
+            creation_date)
+
+		if err != nil {
+			log.Fatalf("Error on go stack insertion: %s", err.Error())
+		}
+
+    }
+
+    return nil
+}
