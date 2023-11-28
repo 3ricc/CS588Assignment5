8ab9a4b7ab3d08af5af312a840c097e812a7d2f7
ESP23SCM70Z <zericcz@gmail.com>
2023-11-28 02:49:39 -0600

    test

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "database/sql"
+    //"encoding/json"
+    "os"
+    "net/http"
+    _ "github.com/lib/pq"
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
+    _ "github.com/google/go-github/v56/github"
+)
+
+func main() {
+    connectionName := "cs588assignment5-406403:us-central1:mypostgres"
+	dbUser := "postgres"
+	dbPass := "root"
+	dbName := "cs588_assignment_5"
+
+    dbURI := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",
+		connectionName, dbName, dbUser, dbPass)
+
+    log.Println("Initializing database connection")
+	db, err := sql.Open("cloudsqlpostgres", dbURI)
+	if err != nil {
+		log.Fatalf("Error on initializing database connection: %s", err.Error())
+	}
+	defer db.Close()
+
+	//Test the database connection
+	log.Println("Testing database connection")
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error on database connection: %s", err.Error())
+	}
+	log.Println("Database connection established")
+
+	log.Println("Database query done!")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+        port = "8080"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello, world!"))
+    })
+	go func() {
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	}()
+
+    collectGithubData(db)
+
+
+}
+
+func collectGithubData (db *sql.DB) error {
+
+}
+
+func collectStackOverflowData (db *sql.DB) error{
+
+}
